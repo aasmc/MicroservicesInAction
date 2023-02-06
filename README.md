e43d0d5c8628a65dcaffa11963b4d32f7e933110
Alex Alexeyev <aasmc@yandex.ru>
2023-02-06 22:01:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Microservices In Action
+
+Educational project based on the book Spring Microservices in Action
+2-nd edition. 
---
+++ src/test/java/ru/aasmc/mia/MicroservicesInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aasmc.mia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
