acab68cddd1c1f2e437b2f4371f74aafc6ba6e95
aleksas <aasmc@yandex.ru>
2023-02-17 16:10:17 +0300

    add module for eureka server

---
+++ eurekaserver/src/test/java/ru/aasmc/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aasmc.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
