ad6c9583db0cef24c041df2cbdc44281e9c70e15
aleksas <aasmc@yandex.ru>
2023-02-17 16:12:00 +0300

    add dockerfile to eureka server

--- eurekaserver/src/main/java/ru/aasmc/eurekaserver/EurekaServerApplication.java
+++ eurekaserver/src/main/java/ru/aasmc/eurekaserver/EurekaServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EurekaserverApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaserverApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 
 }
